ce0bb813d68753a4e222609acbc8fab08d53a127
Umut Ramazan Eği <78850577+umutramazanegi@users.noreply.github.com>
2025-04-06 14:35:22 +0300

    MVC PROJESI

---
+++ MvcCvProjesi/MvcCvProjesi/Controllers/DefaultController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvProjesi.Models.entity;
+namespace MvcCvProjesi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbCvEntities db = new DbCvEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyim = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyim);
+        }
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TblEgitimlerim.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYetenek.ToList();
+            return PartialView(yetenekler);
+        }
+
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifikalar = db.TblSertifikalarim.ToList();
+            return PartialView(sertifikalar);
+        }
+        [HttpGet]
+        
+        public PartialViewResult İletisim()
+        {
+
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult İletisim(Tbliletisim p)
+        {
+            p.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Tbliletisim.Add(p);
+            db.SaveChanges();
+            return PartialView();
+        }
+        //public PartialViewResult SosyalMedya()
+        //{
+        //    var medya = db.TblSosyalMedya.Where(x => x.Durum == true).ToList();
+        //    return PartialView(medya);
+        //}
+    }
+}
